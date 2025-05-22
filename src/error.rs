10a5c6c2057079536abdb2ec674978510d6cc80a
Cyber Phantom <inam123451@gmail.com>
2025-05-23 00:14:41 +0530

    Add custom error type

---
+++ src/error.rs
@@ -0,0 +1,28 @@
+use std::error;
+
+#[derive(Debug)]
+pub enum Error {
+    UnexpectedCharacter(char),
+    UnexpectedToken(String),
+    LexingError { lexeme: String, line: usize },
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let msg = match self {
+            Error::UnexpectedCharacter(c) => {
+                format!("Unexpected character: {}", c)
+            }
+            Error::UnexpectedToken(s) => {
+                format!("Unexpected token: {}", s)
+            }
+            Error::LexingError { lexeme, line } => {
+                format!("[line {}]: Lexing error: {}", line, lexeme)
+            }
+        };
+
+        write!(f, "{}", msg)
+    }
+}
+
+impl error::Error for Error {}
