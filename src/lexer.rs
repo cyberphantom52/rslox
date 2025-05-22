71c4a6d574c46cefdcd90941a52b9c310a2a7e26
Cyber Phantom <inam123451@gmail.com>
2025-05-22 22:51:32 +0530

    lexer: Add new() method

--- src/lexer.rs
+++ src/lexer.rs
@@ -4,4 +4,14 @@ enum Token {
     Operator,
 }
 
-struct Lexer {}
+struct Lexer {
+    lines: Vec<String>,
+}
+
+impl Lexer {
+    pub fn new(stream: String) -> Self {
+        Self {
+            lines: stream.lines().map(|line| line.to_string()).collect(),
+        }
+    }
+}
