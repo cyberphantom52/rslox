73dc46bebaf81bdb1c4af50c3361a2734f8e08a1
Cyber Phantom <inam123451@gmail.com>
2025-05-26 00:36:13 +0530

    Add commandline parsing using clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
 mod error;
 mod lexer;
 mod token;
+use std::path::PathBuf;
 
-fn main() {}
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Tokenize { filename: PathBuf },
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args.command {
+        Command::Tokenize { filename } => {
+            let content = std::fs::read_to_string(&filename).expect("Failed to read the file");
+            let lexer = lexer::Lexer::new(content);
+            for token in lexer.consume() {
+                println!("{:?}", token);
+            }
+        }
+    }
+}
