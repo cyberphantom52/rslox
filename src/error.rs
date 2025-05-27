eb5b946acd6c2e69c082b6d707a2bc63fbd22536
Cyber Phantom <inam123451@gmail.com>
2025-05-27 13:13:13 +0530

    error: Fix error msg for strings

--- src/error.rs
+++ src/error.rs
@@ -15,7 +15,7 @@ pub enum LexingError {
 impl std::fmt::Display for LexingError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            LexingError::UnterminatedString => write!(f, "Unterminated string literal"),
+            LexingError::UnterminatedString => write!(f, "Unterminated string."),
             LexingError::UnexpectedCharacter(c) => write!(f, "Unexpected character: {c}"),
         }
     }
